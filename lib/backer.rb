887a240eca2ac2ef433b1d5bbf5e145d75be6c37
Riley Moore <rilemoore@gmail.com>
2019-10-23 20:54:19 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer
+  
+  attr_reader :backed_projects
+  
+  def initialize
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+  end
+  
+end
---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  
+  attr_reader :backers
+  
+  def initialize
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+  end
+  
+end
